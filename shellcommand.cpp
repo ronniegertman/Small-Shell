7bb12c390e75e3a6bd44a99a09ff268326b46b50
yair-dardik <yairdardik@gmail.com>
2025-12-07 21:18:21 +0200

    Fix alias handling in addAlias to overwrite existing aliases

--- shellcommand.cpp
+++ shellcommand.cpp
@@ -9,7 +9,17 @@ Alias::Alias(std::string originalcmd, std::string aliasedcmd) : originalcmd(std:
 aliasedcmd(std::move(aliasedcmd)){}
 
 void AliasedCmds::addAlias(std::string original, std::string aliased){
-	Alias toadd(original, aliased);
+	if (isAlias(aliased)) {
+        // 2. If it exists, find it and overwrite the command definition
+        for (auto &aliasObj : aliasedList) {
+            if (aliasObj.aliasedcmd == aliased) {
+                aliasObj.originalcmd = original;
+                return;
+            }
+        }
+    }
+    
+    Alias toadd(original, aliased);
 	aliasedList.push_back(toadd);
 }
 
